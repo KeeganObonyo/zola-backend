886bf3a3b0d1cd970f4e41c93fd8bc9313f53ef2
KeeganObonyo <keeganobonyo@gmail.com>
2024-04-21 19:14:47 +0300

    MAIN - The schemas for PostgresQL and Cassandra to be used

---
+++ data/mysql/schema.sql
@@ -0,0 +1,40 @@
+DROP DATABASE IF EXISTS `zola`;
+CREATE DATABASE `zola` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci;
+USE `zola`;
+
+--
+-- Table structure for table `user`
+--
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `username` varchar(32) NOT NULL,
+  `first_name` varchar(32) NOT NULL,
+  `last_name` varchar(32) NOT NULL,
+  `business_name` varchar(32) NOT NULL,
+  `text` varchar(32) NOT NULL,
+  `place_id` varchar(32) NOT NULL,
+  `api_key` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `card_holder` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `username` varchar(32) NOT NULL,
+  `first_name` varchar(32) NOT NULL,
+  `last_name` varchar(32) NOT NULL,
+  `business_name` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;

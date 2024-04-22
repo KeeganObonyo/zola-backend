37ecd1c528e23b67527d0ef515d8a7e40f7a8884
Keegan Obonyo <35022901+KeeganObonyo@users.noreply.github.com>
2024-04-22 00:40:04 -0700

    Update schema.sql

--- data/mysql/schema.sql
+++ data/mysql/schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE `user` (
   `first_name` varchar(32) NOT NULL,
   `last_name` varchar(32) NOT NULL,
   `business_name` varchar(32) NOT NULL,
-  `text` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
   `place_id` varchar(32) NOT NULL,
   `api_key` varchar(64) NOT NULL,
   PRIMARY KEY (`id`),

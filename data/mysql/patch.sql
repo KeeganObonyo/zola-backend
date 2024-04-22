52df934758436a92aedfcc9e3173631b6e4bc126
KeeganObonyo <keeganobonyo@gmail.com>
2024-04-22 17:11:05 +0300

    MAIN - Modified sql schema. N/B Separate user and API user for separation of concerns

---
+++ data/mysql/patch.sql
@@ -0,0 +1,2 @@
+INSERT INTO `api_user` (`username`, `api_key`) VALUES ('testuser1', SHA2('testpass1', 256));
+INSERT INTO `api_user` (`username`, `api_key`) VALUES ('testuser2', SHA2('testpass2', 256));
